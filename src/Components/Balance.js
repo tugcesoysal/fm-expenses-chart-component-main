f85c8b5f57776da29ccc3c9a8260df17736e6eee
tugcesoysal <zeyneptugcesoysal@gmail.com>
2023-11-23 21:25:53 -0500

    Configure React app for deployment to GitHub Pages

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -5,7 +5,12 @@ export default function Balance() {
         <p className="my_balance_parag">My balance</p>
         <h1 className="balance_data">$921.48</h1>
       </div>
-      <img src="/logo.svg" alt="logo" width={80} className="logo" />
+      <img
+        src={process.env.PUBLIC_URL + "/logo.svg"}
+        alt="logo"
+        width={80}
+        className="logo"
+      />
     </div>
   );
 }
